54d6bbfeed859560b26bbaa86a6f3f2ad411d96c
Nitish09mishra <42340130+Nitish09mishra@users.noreply.github.com>
2020-06-03 12:23:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function exec() {
 	document.querySelectorAll(".dice img")[1].setAttribute("src", "images/dice"+randomNumber2+".png");
 
 	if(randomNumber1 > randomNumber2){
-		document.querySelector(".container h1").textContent = "Sanju won ⛳";
+		document.querySelector(".container h1").textContent = "Person 1 won ⛳";
 	} 
 	else if(randomNumber1 < randomNumber2){
-		document.querySelector(".container h1").textContent = "Maddy won 🤟";
+		document.querySelector(".container h1").textContent = "Person 2 won 🤟";
 	} 
 	else {
 		document.querySelector(".container h1").textContent = "Draw 😐";
